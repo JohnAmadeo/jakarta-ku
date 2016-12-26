7d1ba96303ce99d7cf65b0f9903eecdfec5ef705
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2016-12-26 09:18:49 -0500

    Add wireframe for header, categories

--- static/js/Main.js
+++ static/js/Main.js
@@ -10,7 +10,67 @@ class Main extends React.Component {
   render() {
     return (
       <div className='Main'>
-        <JakartaMap />
+        <Header />
+        <InfoBar />
+        {/*<Display />*/}
+      </div>
+    )
+  }
+}
+
+const Header = (props) => {
+  return (
+    <div className='Header'>
+      <nav className="navbar navbar-fixed-top">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">JakartaKu</a>
+          </div>
+      </nav>
+    </div>
+  )
+}
+
+class InfoBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="InfoBar">
+        <div className="container">
+          <CategoryList />
+          <div>
+            {/*<Searchbar />*/}
+            asdf
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class CategoryList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="CategoryList">
+        <button type="button" className="btn btn-lg btn-default">
+          &#43; Education
+        </button>
+        <button type="button" className="btn btn-lg btn-default">
+          &minus; Demographics
+        </button>
+        <button type="button" className="btn btn-lg btn-default">
+          &#43; Religion
+        </button>
+        <button type="button" className="btn btn-lg btn-default">
+          &#43; Occupation
+        </button>
+        <button type="button" className="btn btn-lg btn-default">
+          &#43; Marriage
+        </button>
       </div>
     )
   }
