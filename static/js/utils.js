b4dbff26418f2a49c69c13f1bd3022c4fc2c4f13
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-01-07 00:14:11 +0900

    Added new color pallete (background, border and hover)

--- static/js/utils.js
+++ static/js/utils.js
@@ -32,7 +32,41 @@ var utils = {
   getKeys: function() {
     Object.keys(this).filter((key) => key != "getKeys")
                      .map((key) => {console.log(key);})
-  }
+  },
+  getColorPalette: function(num_colors) {
+    const bg_colors = [
+      'rgba(255, 131, 131, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(99, 234, 255, 0.2)',
+      'rgba(252, 110, 63, 0.2)',
+      'rgba(79, 247, 135, 0.2)',
+      'rgba(96, 120, 255, 0.2)',
+    ];
+    const border_colors = [
+      'rgba(255,131,131,1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+      'rgba(99, 234, 255, 1)',
+      'rgba(252, 110, 63, 1)',
+      'rgba(79, 247, 135, 1)',
+      'rgba(96, 120, 255, 1)',
+    ];
+
+    return Array(num_colors).fill('')
+                   .map(function(elem, index) {
+                    return {
+                      background: bg_colors[index % num_colors],
+                      border: border_colors[index % num_colors]
+                    }
+                   });
+  }  
 }
 
 module.exports = utils;
