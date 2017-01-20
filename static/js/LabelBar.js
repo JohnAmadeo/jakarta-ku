0489425b878640307f28c497f43835b486c226c0
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-01-20 04:24:46 -0500

    Remove references to unused superagent library

--- static/js/LabelBar.js
+++ static/js/LabelBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import Store from 'store2';
-import Request from 'superagent';
 import Axios from 'axios';
 import Utils from './utils';
 
