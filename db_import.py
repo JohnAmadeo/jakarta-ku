edefb0966be5ee2b5b8bc6c0ee38c5efb37ef5a8
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-01-20 05:50:56 -0500

    Add file to import dataset CSVs as MongoDB collections

---
+++ db_import.py
@@ -0,0 +1,27 @@
+import pymongo
+import sys, os, json
+import pandas as pd
+
+DB = pymongo.MongoClient("mongodb://heroku_1r8z46f3:lgjti45im49sf0pnhshhghldc2@ds117889.mlab.com:17889/heroku_1r8z46f3", 
+                         connectTimeoutMS=30000,
+                         socketTimeoutMS=None,
+                         socketKeepAlive=True)['jakartaku']
+
+def main():
+    folder = 'static/csv/'
+    import_content(folder + 'religion2013_clean.csv','religion')
+    import_content(folder + 'occupation2013_clean.csv','occupation')
+    import_content(folder + 'demographics2014_clean.csv','demographics')
+    import_content(folder + 'education2014_clean.csv','education')
+    import_content(folder + 'marriage2015_clean.csv','marriage')
+
+def import_content(filepath, collection_name):
+    global DB
+    collection = DB[collection_name]
+    data = pd.read_csv(filepath)
+    data_json = json.loads(data.to_json(orient='records'))
+    collection.remove()
+    collection.insert(data_json)
+
+if __name__ == "__main__":
+    main()
